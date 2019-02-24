4c9901cc0f5e60e6f560556f89a605d851a9f2d7
sugimount <sugi.mount@gmail.com>
2019-02-24 20:01:00 +0900

    fist commit

---
+++ compartment.go
@@ -0,0 +1,17 @@
+package fn-oci-lb-list
+
+import (
+	"fmt"
+	"os"
+)
+
+//GetCompartmentID 環境変数からCompartmentIDを取得する
+func GetCompartmentID() (value string, err error) {
+	envKey := "OCI_COMPARTMENT_ID"
+	var ok bool
+	if value, ok = os.LookupEnv(envKey); !ok {
+		err = fmt.Errorf("can not read CompartmentID from environment variable %s", envKey)
+		return "", err
+	}
+	return value, nil
+}
